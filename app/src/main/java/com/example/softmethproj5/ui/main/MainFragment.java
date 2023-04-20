b9df4e013596c7e22aa1845c81d67343dbefca55
VikrantPulipati <vikrant.pulipati@gmail.com>
2023-04-20 17:12:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/softmethproj5/ui/main/MainFragment.java
@@ -0,0 +1,48 @@
+package com.example.softmethproj5.ui.main;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.softmethproj5.R;
+import com.example.softmethproj5.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    FragmentMainBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+
+
+        // TODO: Use the ViewModel
+    }
+
+}
